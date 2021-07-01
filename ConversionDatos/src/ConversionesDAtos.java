480b216441a63196b4d1581e8726a438f34c95f9
Luis Bucio <luis.mtz.bucio@gmail.com>
2021-07-01 11:26:26 -0500

    Primer commit

---
+++ ConversionDatos/src/ConversionesDAtos.java
@@ -0,0 +1,26 @@
+
+public class ConversionesDAtos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * Tipos de datos Java
+		 * Numericos: byte, short, int, long, float,double
+		 * Boolean:(verdadero,falso): boolean
+		 * Caracteres: char
+		 * Cadenas de texto: String
+		 * */
+		
+		/*solo se puede asignar a numero más grandes la convesion de tipo de 
+		 * dato tambien se le conoce como casteo*/
+		
+		int numero1 = 1000;
+		long numero2 = numero1;
+		float numero3 = numero2;
+		
+		System.out.println(numero1);
+		System.out.println(numero2);
+		System.out.println(numero3);
+	}
+
+}
